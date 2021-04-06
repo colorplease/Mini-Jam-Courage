c8bd42e69af070f3e180ffbf964dfc0d6151f5af
colorplease <alistairvtran@gmail.com>
2021-04-06 09:21:55 -0700

    Graphical Update Part 1

--- Mini Jam Courage/Assets/Scripts/Menu.cs
+++ Mini Jam Courage/Assets/Scripts/Menu.cs
@@ -7,6 +7,17 @@ public class Menu : MonoBehaviour
 {
 
     public Animator animator;
+
+    public GameObject godhimself;
+
+    public GameObject wegot;
+    public GameObject wegot2;
+    public GameObject wegot3;
+    public GameObject wegot4;
+
+    public GameObject Credits2;
+
+    public GameObject hmf;
     public void StartGame()
     {
         StartCoroutine(gamers());
@@ -22,10 +33,31 @@ public class Menu : MonoBehaviour
         StartCoroutine(gaming());
     }
 
+    public void gamers1()
+    {
+        Credits2.SetActive(true);
+        hmf.SetActive(false);
+
+    }
+
+    public void ungamers()
+    {
+        Credits2.SetActive(false);
+        hmf.SetActive(true);
+    }
+
+  
+
     IEnumerator gamers()
     {
         animator.SetBool("audio", true);
-        yield return new WaitForSeconds(2f);
+        godhimself.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        wegot.SetActive(true);
+        wegot2.SetActive(true);
+        wegot3.SetActive(true);
+        wegot4.SetActive(true);
+        yield return new WaitForSeconds(4f);
          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
